4b685de757c9eab3278851a62e0f762e0dcf5d2e
Arescarpio <Arescarpio@gmail.com>
2018-03-28 11:25:32 -0700

    Add files via upload
    having issue installing the axios on console where I cannot test. But i tried to at least change my states for API

--- src/App.js
+++ src/App.js
@@ -4,22 +4,40 @@ import './App.css';
 import Form from './Form';
 import Login from './login';
 import Card from './Card';
+import axios from 'axios';
+
 
 class App extends Component {
+
+   
+    state = {spots: []}
+    componentDidMount()
+    {
+        axios.get(`https://airgara.ge/api/spots/`)
+            .then(res => {
+                const spots = res.data;
+                this.setState({ spots });
+            })
+    }
+
     render() {
         return (
             <div className="App">
                 <h1 classname="App-title">AirGarage</h1>
-             
-                <Card></Card>
 
-      
-                
-               
+                <div>
+                {this.state.spots.map(spot => <Card
+
+     
+                    spotName={spot.Spotname}
+                    spotPrice={spot.spotPrice}
+                    spotAddress={spot.spotAddress}
+                    spotQuantity={spot.spotQuantity}
+                    spotAvailable={spot.spotAvailiable}   />)}
+             
+            </div> 
             </div>
-          
-      
-          
+     
         );
     }
 }
--- src/Card.js
+++ src/Card.js
@@ -6,11 +6,11 @@ class Card extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            spotName: '',
-            spotPrice: '',
-            spotAddress : '',
-            spotQuantity: '',
-            spotAvailable: '',
+            spotName: props.spotName,
+            spotPrice: props.spotPrice,
+            spotAddress: props.spotAddress,
+            spotQuantity: props.spotQuantity,
+            spotAvailable: props.spotAvailable,
             spotPic: require('./logoPic.png')
         }
     }
@@ -23,135 +23,38 @@ class Card extends React.Component {
                 <div className='cardSplit'>
                     <img src={this.state.spotPic} className="logoPic" alt="aglogo" />/>
                     <div>
-                          <td>{this.state.spotName = 'Ares Parking Spot'}</td>
+                          <td>{this.props.spotName}</td>
                         <br />   
                     </div>
 
                     <div>
                         <th>Price Per Month: </th>
-                        <td>{this.state.spotPrice = '$10000'}</td>
+                        <td>{this.props.spotPrice}</td>
                         <br />   
                     </div>
 
                     <div>
                         <th>Location Address: </th>
-                        <td>{this.state.spotAddress = '1800 E Waffle dr'}</td>
+                        <td>{this.props.spotAddress}</td>
                         <br />  
                     </div>
 
                     <div>
                         <th>Spots Available: </th>
-                        <td>{this.state.spotQuantity = '9'}</td>
+                        <td>{this.props.spotQuantity}</td>
                         <br />   
                     </div>
 
                     <div>
                         <th>Availiable 24/7: </th>
-                        <td>{this.state.spotAvailable = '7:00am - 7:00pm'}</td>
+                        <td>{this.props.spotAvailable}</td>
                         <br />   
                     </div>
                   
                 </div>
+                <br /> 
+                
 
-                <div className='cardSplit'>
-                    <img src={this.state.spotPic} className="logoPic" alt="aglogo" />/>
-                    <div>
-                         <td>{this.state.spotName = 'Billy Joe Parking Spot'}</td>
-                        <br />  
-                    </div>
-
-                    <div>
-                        <th>Price Per Month: </th>
-                        <td>{this.state.spotPrice = '$99'}</td>
-                        <br />   
-                    </div>
-
-                    <div>
-                        <th>Location Address: </th>
-                        <td>{this.state.spotAddress = '1111 N Bob dr'}</td>
-                        <br />   
-                    </div>
-
-                    <div>
-                        <th>Spots Available: </th>
-                        <td>{this.state.spotQuantity = '1'}</td>
-                        <br />  
-                    </div>
-
-                    <div>
-                        <th>Availiable 24/7: </th>
-                        <td>{this.state.spotAvailable = 'Yes'}</td>
-                        <br />   
-                    </div>
-
-                </div>
-
-                <div className='cardSplit'>
-                    <img src={this.state.spotPic} className="logoPic" alt="aglogo" />/>
-                    <div>
-                        
-                        <td>{this.state.spotName = 'Santa Claus Parking Spot'}</td>
-                        <br />   
-                    </div>
-
-                    <div>
-                        <th>Price Per Month: </th>
-                        <td>{this.state.spotPrice = '$0.99'}</td>
-                        <br />   
-                    </div>
-
-                    <div>
-                        <th>Location Address: </th>
-                        <td>{this.state.spotAddress = '1 N Pole dr'}</td>
-                        <br />  
-                    </div>
-
-                    <div>
-                        <th>Spots Available: </th>
-                        <td>{this.state.spotQuantity = '9999'}</td>
-                        <br />   
-                    </div>
-
-                    <div>
-                        <th>Availiable 24/7: </th>
-                        <td>{this.state.spotAvailable = 'Yes'}</td>
-                        <br />   
-                    </div>
-
-                </div>
-                <div className='cardSplit'>
-                    <img src={this.state.spotPic} className="logoPic" alt="aglogo" />/>
-                    <div>
-                      
-                        <td>{this.state.spotName = 'Kobe Bryant Parking Spot'}</td>
-                        <br />   
-                    </div>
-
-                    <div>
-                        <th>Price Per Month: </th>
-                        <td>{this.state.spotPrice = '$150'}</td>
-                        <br />   
-                    </div>
-
-                    <div>
-                        <th>Location Address: </th>
-                        <td>{this.state.spotAddress = '24 E Goat dr'}</td>
-                        <br />   
-                    </div>
-
-                    <div>
-                        <th>Spots Available: </th>
-                        <td>{this.state.spotQuantity = '4'}</td>
-                        <br />   
-                    </div>
-
-                    <div>
-                        <th>Availiable 24/7: </th>
-                        <td>{this.state.spotAvailable = 'No'}</td>
-                        <br />  
-                    </div>
-
-                </div>
             </div>
 
         );
